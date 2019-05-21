336304a5da63662cf4fc8b69974ed3d0fbb48475
motoki317 <motoki317@gmail.com>
2019-05-21 18:51:46 +0900

    Server setup

---
+++ init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -eu
+GO111MODULE=on go build -o main
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Server successfully started!")
+	})
+
+	e.Start(":10900")
+}
