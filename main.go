7633fb106b5f838b6bf696f112f833f486454a88
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-22 21:17:11 +0900

    データベースの情報を取得するのを公式ドキュメントの通りに書いてみた

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -12,6 +15,14 @@ func main() {
 
 	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
+	})
+
+	e.GET("/db", func(c echo.Context) error {
+		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return c.JSON(http.StatusOK, db)
 	})
 
 	port := os.Getenv("PORT")
