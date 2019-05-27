97e72a5e269b82abe1eef6cab4d4137a7d46bc37
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-27 18:50:16 +0900

    ユーザーステータスの構造体を作った

--- structure/structure.go
+++ structure/structure.go
@@ -1,1 +1,21 @@
 package structure
+
+type DateInGame struct {
+	// ゲーム内日時
+
+	Year  int // 1or2(1年目、1月以降)
+	Month int
+	Date  int
+}
+
+type CoopStatus struct {
+}
+
+type UserStatus struct {
+	UserName  string
+	UserId    string
+	Password  string
+	UserImage string
+	DateInGame
+	CoopStatus
+}
