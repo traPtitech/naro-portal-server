e44bd02ca63f8965788d9150a9240606d41019b9
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-28 18:18:50 +0900

    sessionまわりをmain.goの方で書いた

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/middleware"
 
 	"github.com/oribe1115/phan-sns-server/handler"
 	"github.com/oribe1115/phan-sns-server/model"
@@ -18,7 +20,15 @@ func main() {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
 
+	store, err := model.StoreForSession()
+	store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(session.Midleware(store))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
