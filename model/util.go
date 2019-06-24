00ab6bf5e0e1c393403109ff47e821abfb6f6fc7
mazrean <shunsuke.w04@gmail.com>
2019-06-24 17:40:21 +0900

    util.go修正

--- model/util.go
+++ model/util.go
@@ -14,7 +14,7 @@ var (
 
 //Establish データベースに接続
 func Establish() error {
-	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("MARIADB_HOSTNAME"), os.Getenv("MARIADB_DATABASE"), os.Getenv("MARIADB_USERNAME"), os.Getenv("MARIADB_PASSWORD")))
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("MARIADB_USERNAME"), os.Getenv("MARIADB_PASSWORD"), os.Getenv("MARIADB_HOSTNAME"), "3333", os.Getenv("MARIADB_DATABASE")))
 	if err != nil {
 		return err
 	}
