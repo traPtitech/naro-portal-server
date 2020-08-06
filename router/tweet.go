1dee19b2c925ccdfbddfc964e69008f29d993f5b
pure_white_404 <jr2hao.23soda@gmail.com>
2020-08-06 13:03:23 +0900

    created_atをサーバで生成するようにした

--- router/tweet.go
+++ router/tweet.go
@@ -6,6 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/purewhite404/naro-server/model"
 	"net/http"
+	"time"
 )
 
 func GetTweetHandler(c echo.Context) error {
@@ -19,20 +20,24 @@ func GetTweetHandler(c echo.Context) error {
 
 func PostTweetHandler(c echo.Context) error {
 	tweet := new(model.JsonTweet)
-	err := c.Bind(tweet)
+	err := c.Bind(tweet) // ここでidとcreated_atはダミー値なので以下の処理においてサーバ側で生成する
 	if err != nil {
 		return c.String(http.StatusBadRequest, "Not suitable for JsonTweet format")
 	}
 
+	// uuidを生成し、tweetのidとする
 	u, err := uuid.NewRandom()
 	if err != nil {
 		return c.String(http.StatusInternalServerError, "Cannot create tweet uuid")
 	}
 	tweet.ID = u.String()
 
+	// created_atを生成しタイムスタンプとする
+	tweet.CreatedAt = time.Now()
+
 	err = model.InsertTweet(tweet)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, fmt.Sprintf("Cannot insert tweet into database: %v", err))
 	}
-	return c.String(http.StatusCreated, "Post tweet successfully")
+	return c.JSON(http.StatusCreated, tweet)
 }
