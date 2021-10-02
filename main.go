436ebae73a39695e52811f7e3ac4b9f74701082e
logica <84759675+logica0419@users.noreply.github.com>
2021-10-02 22:00:46 +0900

    `/ping`エンドポイントを実装

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
 
 func main() {
-	fmt.Println("Q'n'A - traP Anonymous Question Box Service")
+	fmt.Print("Q'n'A - traP Anonymous Question Box Service")
+
+	e := echo.New()
+
+	e.Logger.SetLevel(log.DEBUG)
+	e.Logger.SetHeader("${time_rfc3339} ${prefix} ${short_file} ${line} |")
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} method = ${method} | uri = ${uri} | status = ${status} ${error}\n",
+	}))
+
+	api := e.Group("/api")
+	api.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
+
+	e.Logger.Panic(e.Start(":9000"))
 }
